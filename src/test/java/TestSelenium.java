00ca429f123a5e6506d0e0af7566ffcef7335056
Spartak Ohanjanyan <spartakohanjanyan@gmail.com>
2026-08-25 00:40:07 +0400

    Add selenium homework

---
+++ src/test/java/TestSelenium.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSelenium {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name"))
+                .sendKeys("standard_user");
+
+        driver.findElement(By.id("password"))
+                .sendKeys("secret_sauce");
+
+        driver.findElement(By.id("login-button"))
+                .click();
+
+        driver.findElement(
+                By.id("add-to-cart-test.allthethings()-t-shirt-(red)")
+        ).click();
+    }
+}
