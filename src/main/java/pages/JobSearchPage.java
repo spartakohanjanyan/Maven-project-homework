3765b0ea22633f1ae67b07cd2f294ce8a795d71e
Spartak Ohanjanyan <spartakohanjanyan@gmail.com>
2026-09-15 05:15:01 +0400

    Add TC-2 correction

--- src/main/java/pages/JobSearchPage.java
+++ src/main/java/pages/JobSearchPage.java
@@ -1,81 +1,66 @@
 package pages;
 
-import org.openqa.selenium.*;
+import base.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
 
-public class JobSearchPage {
-    private WebDriver driver;
-    private WebDriverWait wait;
-    private By searchPlaceholderLoc = By.xpath("//input[@placeholder='Enter keywords...']");
-    private By searchBtnLoс = By.xpath("//div[text()='Search']");
-    private By clearFiltersLoc = By.xpath("//div[text()='Clear filters']");
-    private By noJobsMessageLoc =
-            By.xpath("//*[contains(text(), 'Your search returned no results. Please try using different keywords.')]");
-    private By jobTitleText = By.xpath("//img[@alt='left-icon']/following::div[3]");
+public class JobSearchPage extends BasePage {
 
-    public JobSearchPage(WebDriver driver){
-        this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    private static final String JOBS_URL = "https://staff.am/jobs";
+
+    private final By searchPlaceholderLoc = By.xpath("//input[@placeholder='Enter keywords...']");
+    private final By searchBtnLoc = By.xpath("//div[text()='Search']");
+    private final By clearFiltersLoc = By.xpath("//div[text()='Clear filters']");
+    private final By noJobsMessageLoc = By.xpath("//*[contains(text(), 'Your search returned no results. Please try using different keywords.')]");
+    private final By jobTitleText = By.xpath("//img[@alt='left-icon']/following::div[3]");
+
+    public JobSearchPage(WebDriver driver) {
+        super(driver);
     }
-    public void searchForJob(String key) {
-        WebElement searchInput = wait.until(ExpectedConditions.elementToBeClickable(searchPlaceholderLoc));
-        searchInput.clear();
-        searchInput.sendKeys(key);
+
+    public void open() {
+        driver.get(JOBS_URL);
     }
-    public void pressEnter() {
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(searchPlaceholderLoc));
-        element.sendKeys(Keys.ENTER);
+
+    public void enterSearchKeyword(String key) {
+        type(searchPlaceholderLoc, key);
     }
-    public void clickToSearchButton() {
-        scrollToElement(searchBtnLoс);
-        wait.until(ExpectedConditions.elementToBeClickable(searchBtnLoс)).click();
+
+    public void submitSearchWithEnter() {
+        pressEnter(searchPlaceholderLoc);
     }
+
+    public void clickSearchButton() {
+        click(searchBtnLoc);
+    }
+
     public void scrollToSearchInput() {
         scrollToElement(searchPlaceholderLoc);
     }
+
+    public void clearFilters() {
+        click(clearFiltersLoc);
+    }
+
     public boolean isClearFiltersVisible() {
-        try {
-            wait.until(ExpectedConditions.visibilityOfElementLocated(clearFiltersLoc));
-            return true;
-        } catch (TimeoutException e) {
-            return false;
-        }
+        return isVisible(clearFiltersLoc);
     }
+
     public boolean isClearFiltersInvisible() {
-        try {
-            wait.until(ExpectedConditions.invisibilityOfElementLocated(clearFiltersLoc));
-            return true;
-        } catch (TimeoutException e) {
-            return false;
-        }
+        return isInvisible(clearFiltersLoc);
     }
-    public void scrollToTop() {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollTo({top: 0, behavior: 'instant'});");
-    }
-    private void scrollToElement(By locator) {
-        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView({behavior: 'instant', block: 'center'});", element);
-    }
-    public String getText(){
-        return (wait.until(ExpectedConditions.visibilityOfElementLocated(jobTitleText))
-                .getText());
-    }
-    public boolean isDataLoaded() {
-        return wait.until(ExpectedConditions.invisibilityOfElementWithText(jobTitleText, getText()));
-    }
+
     public boolean isNoJobsMessageVisible() {
-        try{
-            wait.until(ExpectedConditions.visibilityOfElementLocated(noJobsMessageLoc));
-            return true;
-        } catch (TimeoutException e){
-            return false;
-        }
+        return isVisible(noJobsMessageLoc);
     }
-    public void clearFilters(){
-        wait.until(ExpectedConditions.visibilityOfElementLocated(clearFiltersLoc)).click();
+
+    public String getFirstJobTitleText() {
+        return getText(jobTitleText);
+    }
+
+    public boolean isDataLoaded() {
+        String currentText = getFirstJobTitleText();
+        return wait.until(ExpectedConditions.invisibilityOfElementWithText(jobTitleText, currentText));
     }
 }
--- src/test/java/JobFindingTest.java
+++ src/test/java/JobFindingTest.java
@@ -6,47 +6,51 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.JobSearchPage;
+import business.JobSearchBusiness;
 
 public class JobFindingTest {
+
     private WebDriver driver;
-    private JobSearchPage jobSearchPage;
+    private JobSearchBusiness jobSearchBusiness;
 
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://staff.am/jobs");
+
+        JobSearchPage jobSearchPage = new JobSearchPage(driver);
+        jobSearchBusiness = new JobSearchBusiness(jobSearchPage);
+
+        jobSearchBusiness.openJobsPage();
         ((JavascriptExecutor) driver).executeScript(
                 "var elem = document.querySelector('.Toastify') || document.querySelector('[class*=\"cookie\"]');" +
                         "if(elem) { elem.remove(); }"
         );
-
-        jobSearchPage = new JobSearchPage(driver);
     }
 
     @Test
     public void jobsSearchPageTest() {
-        jobSearchPage.scrollToSearchInput();
-        jobSearchPage.searchForJob("qa");
-        jobSearchPage.clickToSearchButton();
-        Assert.assertTrue(jobSearchPage.isClearFiltersVisible());
-        Assert.assertTrue(jobSearchPage.isDataLoaded());
-        jobSearchPage.searchForJob("developer");
-        jobSearchPage.pressEnter();
-        Assert.assertTrue(jobSearchPage.isClearFiltersVisible());
-        Assert.assertTrue(jobSearchPage.isDataLoaded());
-        jobSearchPage.searchForJob("tegersef");
-        jobSearchPage.scrollToTop();
-        jobSearchPage.clickToSearchButton();
-        Assert.assertTrue(jobSearchPage.isNoJobsMessageVisible());
-        jobSearchPage.searchForJob("");
-        jobSearchPage.pressEnter();
-        Assert.assertTrue(jobSearchPage.isClearFiltersInvisible());
+
+        jobSearchBusiness.searchForJobWithButton("qa");
+        Assert.assertTrue(jobSearchBusiness.isClearFiltersVisible(), "Clear filters button should be visible");
+        Assert.assertTrue(jobSearchBusiness.isDataLoaded(), "Data should be updated after search");
+
+        jobSearchBusiness.searchForJobWithEnter("developer");
+        Assert.assertTrue(jobSearchBusiness.isClearFiltersVisible(), "Clear filters button should be visible");
+        Assert.assertTrue(jobSearchBusiness.isDataLoaded(), "Data should be updated after search");
+
+        jobSearchBusiness.searchWithScrollToTop("tegersef");
+        Assert.assertTrue(jobSearchBusiness.isNoJobsMessageVisible(), "No jobs message should be displayed");
+
+        jobSearchBusiness.searchForJobWithEnter("");
+        Assert.assertTrue(jobSearchBusiness.isClearFiltersInvisible(), "Clear filters should be invisible for empty search");
     }
 
     @AfterMethod
-    public void closeWebPage(){
-        driver.quit();
-        driver = null;
+    public void closeWebPage() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
     }
 }
